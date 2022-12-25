0f7e30eb78d27e315dc6e5c8c805fad0072b8c30
Thanh Tran <thanh.ptit.96@gmail.com>
2022-12-25 17:42:38 +0900

    Update configs & benchmark script

--- scripts/benchmark_job.sh
+++ scripts/benchmark_job.sh
@@ -9,4 +9,4 @@ cd /home/s2210421/projects/weak-supervision
 
 module load singularity/3.9.5
 
-singularity exec /home/s2210421/docker_images/dev ./scripts/run_bench.sh 10 train_logs/bench
+singularity exec /home/s2210421/docker_images/dev ./scripts/run_bench.sh 20 train_logs/bench
--- scripts/run_bench.sh
+++ scripts/run_bench.sh
@@ -3,15 +3,14 @@
 NUM_TRIALS=${1:-"10"}
 SAVE_DIR=${2:-""}
 
-# python run.py bench configs/imdb/snorkel-base.json -s $SAVE_DIR -n $NUM_TRIALS;
-# python run.py bench configs/imdb/snorkel-rand-abs.json -s $SAVE_DIR -n $NUM_TRIALS;
-# python run.py bench configs/imdb/snorkel-rand-dis.json -s $SAVE_DIR -n $NUM_TRIALS;
-# python run.py bench configs/imdb/nemo-base_1.json -s $SAVE_DIR -n $NUM_TRIALS;
-# python run.py bench configs/imdb/nemo-base.json -s $SAVE_DIR -n $NUM_TRIALS;
+python run.py bench configs/imdb/snorkel-base.json -s $SAVE_DIR -n $NUM_TRIALS;
+python run.py bench configs/imdb/snorkel-rand-abs.json -s $SAVE_DIR -n $NUM_TRIALS;
+python run.py bench configs/imdb/snorkel-rand-dis.json -s $SAVE_DIR -n $NUM_TRIALS;
+python run.py bench configs/imdb/nemo-base.json -s $SAVE_DIR -n $NUM_TRIALS;
 python run.py bench configs/imdb/nemo-full.json -s $SAVE_DIR -n $NUM_TRIALS;
 
 # python run.py bench configs/ytb/snorkel-base.json -s $SAVE_DIR -n $NUM_TRIALS;
 # python run.py bench configs/ytb/snorkel-rand-abs.json -s $SAVE_DIR -n $NUM_TRIALS;
 # python run.py bench configs/ytb/snorkel-rand-dis.json -s $SAVE_DIR -n $NUM_TRIALS;
-python run.py bench configs/ytb/nemo-base.json -s $SAVE_DIR -n $NUM_TRIALS;
-python run.py bench configs/ytb/nemo-full.json -s $SAVE_DIR -n $NUM_TRIALS;
+# python run.py bench configs/ytb/nemo-base.json -s $SAVE_DIR -n $NUM_TRIALS;
+# python run.py bench configs/ytb/nemo-full.json -s $SAVE_DIR -n $NUM_TRIALS;
